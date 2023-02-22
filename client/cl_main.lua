ba3ab55a5580f6e22d2adbdebcc798fbbf3454d4
wolfex <91018450+wolfex01@users.noreply.github.com>
2023-02-22 22:36:55 +0100

    Update cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,14 +1,20 @@
-ESX = nil
-firstSpawn = true
+local ESX = nil
+local datum = ""
+local chance = 0
+local firstSpawn = true
 
+-- Wait for ESX to be ready before executing code
 Citizen.CreateThread(function()
     while ESX == nil do
-        TriggerEvent(Config.SharedObject, function(obj) ESX = obj end)
+        TriggerEvent("esx:getSharedObject", function(obj) 
+            ESX = obj 
+        end)
         Citizen.Wait(0)
         setChance()
     end
 end)
 
+-- Trigger the NUI login panel when the player spawns for the first time
 AddEventHandler("playerSpawned", function(spawn)
     if firstSpawn then
         openNui()
@@ -16,11 +22,13 @@ AddEventHandler("playerSpawned", function(spawn)
     end
 end)
 
+-- Receive the server's date and store it as a string
 RegisterNetEvent('dv_loginpanel:datum')
 AddEventHandler('dv_loginpanel:datum', function(adat)
     datum = tostring(adat)
 end)
 
+-- Open the NUI login panel
 function openNui()
     SetNuiFocus(true, true)
     SendNUIMessage({
@@ -30,6 +38,7 @@ function openNui()
     })
 end
 
+-- Close the NUI login panel
 function closeNui()
     SetNuiFocus(false, false)
     SendNUIMessage({
@@ -37,6 +46,7 @@ function closeNui()
     })
 end
 
+-- Check if the player has reached the maximum number of login attempts
 function countChance()
     if chance >= Config.Chances then 
         setChance()
@@ -44,15 +54,18 @@ function countChance()
     end
 end
 
+-- Reset the number of login attempts
 function setChance()
     chance = 0
 end
 
+-- Show a notification message to the player
 RegisterNetEvent('dv_loginpanel:sendNotify')
 AddEventHandler('dv_loginpanel:sendNotify', function(msg)
-    Notification(msg)
+    ESX.ShowNotification(msg)
 end)
 
+-- Toggle the NUI login panel on or off
 RegisterNetEvent('dv_loginpanel:toggle')
 AddEventHandler('dv_loginpanel:toggle', function(type)
     if type == "be" then
@@ -62,6 +75,7 @@ AddEventHandler('dv_loginpanel:toggle', function(type)
     end
 end)
 
+-- Handle the login response from the server
 RegisterNetEvent('dv_loginpanel:login')
 AddEventHandler('dv_loginpanel:login', function(type, data)
     if type == "success" then 
@@ -78,10 +92,20 @@ AddEventHandler('dv_loginpanel:login', function(type, data)
     end
 end)
 
+-- Handle the login request from the NUI login panel
 RegisterNUICallback('login', function(data)
-    TriggerServerEvent('dv_loginpanel:checkData', data)
+    ESX.TriggerServerCallback('dv_loginpanel:checkData', function(response)
+        if response == "success" then
+            closeNui()
+        else
+            ESX.ShowNotification(response)
+        end
+    end, data)
 end)
 
+-- Handle the registration request from the NUI login panel
 RegisterNUICallback('reg', function(data)
-    TriggerServerEvent('dv_loginpanel:setData', data)
+    ESX.TriggerServerCallback('dv_loginpanel:setData', function(response)
+        ESX.ShowNotification(response)
+    end, data)
 end)
